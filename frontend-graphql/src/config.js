8543d437a9de8d48613e24f1ea7ee01cc0886978
Gabriel Nastase <gabriel.nastase@glintpay.com>
2019-04-29 09:18:25 +0100

    test console log env var

--- frontend-graphql/src/config.js
+++ frontend-graphql/src/config.js
@@ -5,6 +5,7 @@ let url = 'http://localhost:8080/graphql';
 if (process.env.HOME === '/home/node') {
   url = 'http://wp-headless:8080/graphql';
 }
+console.log('node enviroment: ' , process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'production' || process.env.ENV === 'production' ) {
   url = 'http://http://37.251.146.230:8080/graphql';
 }
